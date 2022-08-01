861cfe5ebe01b321a786e8f10cc884ae12ff5fcd
yamadahiroki <hirockysan1983@gmail.com>
2022-08-01 17:57:53 +0900

    ts configの設定変更

--- src/functions/function.ts
+++ src/functions/function.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getBlog = async (setState) => {
+export const getBlog = async () => {
   const req = await axios.get("http://localhost:8888/");
   const res = req.data;
 
